365079341e9751e4a6f42a80bddcb52efb084619
Mike Bruno <mike.bruno@avantcredit.com>
2014-10-31 17:40:04 -0500

    Changed description, added document files, added NEWS.md, changed version number

--- R/prep_sql.R
+++ R/prep_sql.R
@@ -1,17 +1,22 @@
-#' Converts SQL files into a format readable by the run_query() function.
-#' @param path character.  The filepath of the SQL file to be formatted
-#' @return string of SQL query with '--' comments removed and spacing reduced 
-#'  
-
-prep_sql <- function(path) {
+#' Converts .SQL files (or other readable files) into a format readable by the run_query() function.
+#' @param path_to_query character.  The filepath of the SQL file to be formatted.  This should contain the SQL code, can be .SQL or other readable type.
+#' @return string of condensed SQL query with '--' comments removed and spacing reduced 
+#' @examples 
+#' \dontrun{
+#' prep_sql("/path/to/query.SQL")
+#' }
+#' @author Mike Bruno
+#' @export
+prep_sql <- function(path_to_query) {
+  
   # Read The SQL File
-  lines <- readLines(path)
+  lines <- readLines(path_to_query)
   # Remove Comments
   vec <- vapply(lines, gsub, character(1), pattern = "--.*$", replacement = "")
   # Collapse into 1 line
   sql <- paste(vec, collapse=" ")
   # Remove Tabs and Spaces - easier to read
-  sql <- gsub("\t","",sql)
+  sql <- gsub("\t", "", sql)
   sql <- gsub("[[:space:]]+", " ", sql)
   
   sql
