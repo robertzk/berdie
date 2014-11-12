41ac5f68641f5105a2e37c94b8f565c2a134c6d4
Mike Bruno <mike.bruno@avantcredit.com>
2014-11-11 21:41:49 -0600

    Added a check for SQL Code (Instead of a Filepath)

--- R/prep_sql.R
+++ R/prep_sql.R
@@ -8,16 +8,28 @@
 #' @author Mike Bruno
 #' @export
 prep_sql <- function(path_to_query) {
-  
-  # Read The SQL File
-  lines <- readLines(path_to_query)
-  # Remove Comments
-  vec <- vapply(lines, gsub, character(1), pattern = "--.*$", replacement = "")
-  # Collapse into 1 line
-  sql <- paste(vec, collapse=" ")
+  # Check to see if the Path is actually a query.  If not, parse the file.
+  if (test_is_sql(path_to_query) == FALSE) {
+   # Read The SQL File
+   lines <- readLines(path_to_query)
+   # Remove Comments
+   vec <- vapply(lines, gsub, character(1), pattern = "--.*$", replacement = "")
+   # Collapse into 1 line
+   sql <- paste(vec, collapse=" ")
+ } else {
+   sql <- path_to_query
+ }
   # Remove Tabs and Spaces - easier to read
   sql <- gsub("\t", "", sql)
   sql <- gsub("[[:space:]]+", " ", sql)
   
   sql
+}
+
+test_is_sql <- function(test_string) {
+  # Tests for the words "SELECT" and "FROM"
+  result <- grepl("SELECT", test_string, ignore.case = TRUE)
+  result <- result & grepl("FROM", test_string, ignore.case = TRUE)
+  
+  result 
 }
