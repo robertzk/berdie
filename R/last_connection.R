ebee929370c09194f61f7cc498c091d2b5025125
RobertZK <technoguyrob@gmail.com>
2015-02-23 12:59:12 -0600

    use syberiaStructure::syberia_root for disambiguation

--- R/last_connection.R
+++ R/last_connection.R
@@ -4,7 +4,7 @@
 #'   If none exists, the option \code{berdie.database.yml} will be used to
 #'   attempt to load one from a database.yml file. If this option or file
 #'   does not exist, and a Syberia project is currently loaded, it will
-#'   attempt to use the current \code{syberia_root()} to find the relative
+#'   attempt to use the current \code{syberiaStructure::syberia_root()} to find the relative
 #'   \code{config/database.yml} file. Otherwise, it will
 #'   return \code{NULL}.
 #' @seealso \code{\link{postgresql_connection}}
@@ -14,7 +14,7 @@ last_connection <- function() {
   postgresql_connection(getOption('berdie.database.yml'), strict = FALSE) %||%
   (if ('syberia' %in% loaded_packages())
      postgresql_connection(strict = FALSE,
-       file.path(syberia_root(), 'config', 'database.yml'))
+       file.path(syberiaStructure::syberia_root(), 'config', 'database.yml'))
    else NULL)
 }
 
